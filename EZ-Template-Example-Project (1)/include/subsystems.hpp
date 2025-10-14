5404cea18c695644bf0a2985ff3d223c029e72c1
SpruceParsley80 <slugbug427@gmail.com>
2025-10-14 16:23:03 -0700

    why does our intake have pistons on it? it's in order to [CLASSIFIED]. Pretty interesting, huh?

--- EZ-Template-Example-Project (1)/include/subsystems.hpp
+++ EZ-Template-Example-Project (1)/include/subsystems.hpp
@@ -11,6 +11,11 @@ extern pros::Motor intake2(10);
 extern pros::Motor intake3(9);
 extern pros::Motor intake4(21);
 
+//pneumatics
+//scraper
+extern pros::adi::Pneumatics scrape(2);
+//weird lifting intake thing
+extern pros::adi::Pneumatics lift(3); //its probably made out of 2 pistons but still
 
 
 // Your motors, sensors, etc. should go here.  Below are examples
